3829d40a3ec984db07fda838907a9f3a298fe671
Philip Taylor <ptaylor@taylorworld.one>
2019-11-25 19:18:57 +0000

    failed implementation of "val sharedPref = PreferenceManager.getDefaultSharedPreferences(context)".

--- tw11/src/main/java/com/taylorworld/tw11/MiscFragment.kt
+++ tw11/src/main/java/com/taylorworld/tw11/MiscFragment.kt
@@ -3,6 +3,7 @@ package com.taylorworld.tw11
 import android.content.Context
 import android.net.Uri
 import android.os.Bundle
+import android.preference.PreferenceManager
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,6 +13,7 @@ import kotlinx.android.synthetic.main.fragment_misc.*
 import com.google.android.gms.ads.MobileAds
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
+import kotlinx.android.synthetic.main.fragment_title.*
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -103,5 +105,106 @@ class MiscFragment : androidx.fragment.app.Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+    public fun saveAC(view: View) {
+        val armorclass = Integer.parseInt(editText8.text.toString())
+        // this should be getPreferences
+        val sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+        with(sharedPref.edit()) {
+            putInt("AC", armorclass)
+            apply()
+        }
+    }
+    public fun saveHP(view: View) {
+        val hitpoints = Integer.parseInt(editText10.text.toString())
+        // this should be getPreferences
+        val sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+        with(sharedPref.edit()) {
+            putInt("HP", hitpoints)
+            apply()
+        }
+    }
+    public fun saveINIT(view: View) {
+        val initiative = Integer.parseInt(editText11.text.toString())
+        // this should be getPreferences
+        val sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+        with(sharedPref.edit()) {
+            putInt("initiative", initiative)
+            apply()
+        }
+    }
+    public fun saveFortitude(view: View) {
+        val fortitude = Integer.parseInt(editText12.text.toString())
+        // this should be getPreferences
+        val sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+        with(sharedPref.edit()) {
+            putInt("fortitude", fortitude)
+            apply()
+        }
+    }
+    public fun saveReflex(view: View) {
+        val reflex = Integer.parseInt(editText13.text.toString())
+        // this should be getPreferences
+        val sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+        with(sharedPref.edit()) {
+            putInt("reflex", reflex)
+            apply()
+        }
+    }
+    public fun saveWill(view: View) {
+        val will = Integer.parseInt(editText14.text.toString())
+        // this should be getPreferences
+        val sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+        with(sharedPref.edit()) {
+            putInt("will", will)
+            apply()
+        }
+    }
+    public fun saveSpeed(view: View) {
+        val speed = Integer.parseInt(editText15.text.toString())
+        // this should be getPreferences
+        val sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+        with(sharedPref.edit()) {
+            putInt("speed", speed)
+            apply()
+        }
+    }
+    public fun saveGoldPieces(view: View) {
+        val goldpieces = Integer.parseInt(editText9.text.toString())
+        // this should be getPreferences
+        val sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+        with(sharedPref.edit()) {
+            putInt("goldpieces", goldpieces)
+            apply()
+        }
+    }
+    override fun onResume() {
+        super.onResume()
+
+        val sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+
+        val armorclass = sharedPref.getInt("AC", 0);
+        editText8.setText(Integer.toString(armorclass))
+
+        val hitpoints = sharedPref.getInt("HP", 0);
+        editText10.setText(Integer.toString(hitpoints))
+
+        val init = sharedPref.getInt("initiative", 0);
+        editText11.setText(Integer.toString(init))
+
+        val fortitude = sharedPref.getInt("fortitude", 0);
+        editText12.setText(Integer.toString(fortitude))
+
+        val reflex = sharedPref.getInt("reflex", 0);
+        editText13.setText(Integer.toString(reflex))
+
+        val will = sharedPref.getInt("will", 0);
+        editText14.setText(Integer.toString(will))
+
+        val speed = sharedPref.getInt("speed", 0);
+        editText15.setText(Integer.toString(speed))
+
+        val goldpieces = sharedPref.getInt("goldpieces", 0);
+        editText9.setText(Integer.toString(goldpieces))
     }
 }
--- tw11/src/main/java/com/taylorworld/tw11/TitleFragment.kt
+++ tw11/src/main/java/com/taylorworld/tw11/TitleFragment.kt
@@ -3,6 +3,7 @@ package com.taylorworld.tw11
 import android.content.Context
 import android.net.Uri
 import android.os.Bundle
+import android.preference.PreferenceManager
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,6 +13,7 @@ import kotlinx.android.synthetic.main.fragment_title.*
 import com.google.android.gms.ads.MobileAds
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
+import kotlinx.android.synthetic.main.fragment_misc.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -104,5 +106,100 @@ class TitleFragment : androidx.fragment.app.Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+    public fun saveStr(view: View) {
+        val strength = Integer.parseInt(editText2.text.toString())
+        // this should be getPreferences
+        val sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+        with(sharedPref.edit()) {
+            putInt(getString(R.string.STR), strength)
+            apply()
+        }
+    }
+
+    public fun saveDex(view: View) {
+        val dexterity = Integer.parseInt(editText.text.toString())
+        // this should be getPreferences
+        val sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+        with(sharedPref.edit()) {
+            putInt(getString(R.string.DEX), dexterity)
+            apply()
+        }
+    }
+
+    public fun saveInt(view: View) {
+        val intelligance = Integer.parseInt(editText4.text.toString())
+        // this should be getPreferences
+        val sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+        with(sharedPref.edit()) {
+            putInt(getString(R.string.INT), intelligance)
+            apply()
+        }
+    }
+
+    public fun saveWis(view: View) {
+        val wisdom = Integer.parseInt(editText3.text.toString())
+        // this should be getPreferences
+        val sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+        with(sharedPref.edit()) {
+            putInt(getString(R.string.WIS), wisdom)
+            apply()
+        }
+    }
+
+    public fun saveCha(view: View) {
+        val charisma = Integer.parseInt(editText5.text.toString())
+        // this should be getPreferences
+        val sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+        with(sharedPref.edit()) {
+            putInt(getString(R.string.CHA), charisma)
+            apply()
+        }
+    }
+
+    public fun saveCon(view: View) {
+        val constitution = Integer.parseInt(editText6.text.toString())
+        // this should be getPreferences
+        val sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+        with(sharedPref.edit()) {
+            putInt(getString(R.string.CON), constitution)
+            apply()
+        }
+    }
+
+    public fun saveChr(view: View) {
+        val chrname = editText7.text.toString()
+        // this should be getPreferences
+        val sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+        with(sharedPref.edit()) {
+            putString(getString(R.string.CHR), chrname)
+            apply()
+        }
+    }
+    override fun onResume() {
+        super.onResume()
+
+        val sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+
+        val mystr = sharedPref.getInt(getString(R.string.STR), 0);
+        editText2.setText(Integer.toString(mystr))
+
+        val mydex = sharedPref.getInt(getString(R.string.DEX), 0);
+        editText.setText(Integer.toString(mydex))
+
+        val myint = sharedPref.getInt(getString(R.string.INT), 0);
+        editText4.setText(Integer.toString(myint))
+
+        val mywis = sharedPref.getInt(getString(R.string.WIS), 0);
+        editText3.setText(Integer.toString(mywis))
+
+        val mycon = sharedPref.getInt(getString(R.string.CON), 0);
+        editText6.setText(Integer.toString(mycon))
+
+        val mycha = sharedPref.getInt(getString(R.string.CHA), 0);
+        editText5.setText(Integer.toString(mycha))
+
+        val mychr = sharedPref.getString(getString(R.string.CHR), null);
+        editText7.setText(mychr)
     }
 }
